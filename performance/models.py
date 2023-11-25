cb59e553b72b4b5d8b1641349b3980ca3fd1edc4
code-rishav <raj08rishav@gmail.com>
2023-11-25 15:08:35 +0530

    finished performance models

---
+++ performance/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from vendor.models import Vendor
+from order.models import PurchaseOrder
+# Create your models here.
+class Performance(models.Model):
+    vendor = models.ForeignKey(Vendor,on_delete=models.DO_NOTHING)
+    date = models.DateTimeField(auto_now_add=True)
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+    def __str__(self):
+        return self.vendor.name +"-"+ self.date
+
