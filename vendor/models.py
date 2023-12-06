28d2545dbc20e0491a11d3a7c0979f031f922b78
code-rishav <raj08rishav@gmail.com>
2023-12-06 11:20:53 +0530

    updated models.py

--- vendor/models.py
+++ vendor/models.py
@@ -7,11 +7,10 @@ class Vendor(models.Model):
     contact_details = models.TextField()
     address = models.TextField()
     vendor_code = models.CharField(max_length=100,primary_key=True)
-    on_time_delivery_rate = models.FloatField(default=0,blank=True)
-    quality_rating_avg = models.FloatField(default=0,blank=True)
-    average_response_time = models.FloatField(default=0,blank=True)
-    fulfillment_rate = models.FloatField(default=0,blank=True)
+    on_time_delivery_rate = models.FloatField(default=0.0,null=True)
+    quality_rating_avg = models.FloatField(default=0.0,null=True)
+    average_response_time = models.FloatField(default=0.0,null=True)
+    fulfillment_rate = models.FloatField(default=0.0,null=True)
 
     def __str__(self):
-        return self.name + self.vendor_code
-    
+        return self.name + self.vendor_code    
