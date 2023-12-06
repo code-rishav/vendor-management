5b5a601e5a270ca879c71c107ceb0df2a31ce4d7
code-rishav <raj08rishav@gmail.com>
2023-12-06 22:31:24 +0530

    updated test modules

--- order/tests/test_views.py
+++ order/tests/test_views.py
@@ -1,13 +1,13 @@
-import json
 from rest_framework import status
 from django.test import TestCase,client
-from rest_framework.test import APIRequestFactory,APIClient,force_authenticate
+from rest_framework.test import APIRequestFactory,force_authenticate
 from django.urls import reverse
 from vendor.models import Vendor
 from ..models import PurchaseOrder
 from ..serializers import PurchaseOrderSerializer
 from ..views import PurchaseOrderViewset
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 factory = APIRequestFactory()
 view = PurchaseOrderViewset.as_view({'get':'list','post':'create','put':'update','delete':'destroy'})
@@ -18,11 +18,11 @@ class VendorViewTest(TestCase):
     def setUp(self):
         vendor = Vendor.objects.create(vendor_code='testvendorcode1',name='test_vendor1',password='password1', contact_details='contact details',address='address')
         PurchaseOrder.objects.create(po_number='testpo1',vendor=vendor,items={'name':'ABC','price':'123',},quantity=4,status='pending')
-        PurchaseOrder.objects.create(po_number='testpo2',vendor=vendor,items={'name':'ABCd','price':'1234',},quantity=5,status='pending')
+        PurchaseOrder.objects.create(po_number='testpo2',vendor=vendor,items={'name':'ABCD','price':'1234',},quantity=5,status='pending')
     
     def test_get_all_po(self):
         #get API response
-        request = factory.get('/purchaseorder/')
+        request = factory.get('/purchase_orders/')
         force_authenticate(request, user=user)
         response = view(request)
         vendor = PurchaseOrder.objects.all()
@@ -30,28 +30,25 @@ class VendorViewTest(TestCase):
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
     
-    def test_get_detail_vendor(self):
-        request = factory.get('/purchaseorder/testpo1/')
+    def test_get_detail_po(self):
+        request = factory.get('/purchase_orders/testpo1/')
         force_authenticate(request,user=user)
         response = view(request)
         vendor = PurchaseOrder.objects.get(pk='testpo1')
         serializer = PurchaseOrderSerializer(vendor,many=False)
-        #self.assertEqual(response.data[0],serializer.data.get('name'))
         response_data = response.data
         serializer_data = serializer.data
-        #self.assertEqual(response.data[0],serializer.data['name'])
-        #self.assertEqual(response_data[0]['vendor_code'],serializer_data.get('vendor_code'))
         self.assertEqual(response_data[0],serializer_data)
         self.assertEqual(response.status_code,status.HTTP_200_OK)
     
-    def test_post_detail_vendor(self):
+    def test_post_detail_po(self):
         po_object = {'po_number':'testpo3','vendor':'testvendorcode1','items':{'name':'ABC','price':'123'},'quantity':4,'status':'pending'}
-        request = factory.post('/purchaseorder/',po_object,format='json')
+        request = factory.post('/purchase_orders/',po_object,format='json')
         force_authenticate(request,user=user)
         response = view(request)
         self.assertEqual(response.status_code,status.HTTP_201_CREATED)
 
-    def test_put_detail_vendor(self):
+    def test_put_detail_po(self):
         po = PurchaseOrder.objects.get(pk='testpo2')
 
         
@@ -64,17 +61,26 @@ class VendorViewTest(TestCase):
     }
         serializer = PurchaseOrderSerializer(instance=po,data=updated_data)
         if serializer.is_valid():
-            request = factory.put('/purchaseorder/testpo2/',data=serializer.data,format='json')
+            request = factory.put('/purchase_orders/testpo2/',data=serializer.data,format='json')
             force_authenticate(request,user=user)
             response = view(request,pk='testpo2')
-            print("Response data",response)
             self.assertEqual(response.status_code,status.HTTP_200_OK)
         else:
            self.fail(f"Serializer not valid: {serializer.errors}")
 
-    def test_delete_vendor(self):
-        request = factory.delete('/purchaseorder/testpo2/')
+    def test_delete_po(self):
+        request = factory.delete('/purchase_orders/testpo2/')
         force_authenticate(request,user=user)
         response = view(request,pk='testpo2')
-        print(response.status_code)
         self.assertEqual(response.status_code,status.HTTP_204_NO_CONTENT)
+    
+    # def test_status_po(self):
+    #     data = {'acknowledgement_date':timezone.now()}
+    #     url = reverse('acknowledge',kwargs={'pk':'testpo2'})
+    #     request = factory.post(url,data,format='json')
+    #     force_authenticate(request,user)
+    #     response = PurchaseOrderViewset.as_view({'post':'acknowledge'})(request,pk='testpo2')
+    #     print('Response data',response.data)
+    #     print(response.status_code)
+    #     self.assertEqual(response.status_code,status.HTTP_200_OK)
+    #     #self.assertEqual(response.data['acknowledgement_date'] is not None,True)
