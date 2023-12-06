a0db3013167d2e9f6a0633f91734ad59aad87108
code-rishav <raj08rishav@gmail.com>
2023-12-07 01:21:43 +0530

    updated urls.py

--- base/urls.py
+++ base/urls.py
@@ -20,7 +20,6 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
